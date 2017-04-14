be0e0689753ff982ce71772c5c080aa88ce4614b
rohit khatana <rohitkhatana.khatana@gmail.com>
2017-04-14 16:03:09 +0530

    added upload example for load testing

--- locustfile.py
+++ locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpLocust, TaskSet
+from locust import HttpLocust, TaskSet, task
 
 
 class FileServiceBehavior(TaskSet):
@@ -8,6 +8,12 @@ class FileServiceBehavior(TaskSet):
     @task
     def upload(self):
         ''' File service upload test '''
+        file_info = {'file': ('some.pdf', open('/Users/rohitkhatana/Downloads/2312100174779600000.pdf', 'rb'), "application/pdf")}
+        values = {'bucketName': 'bucket', 'host': 'localhost', 'path': 'load_test'}
+        #self.client.post('/v1/policy/AEX6BVWJ7D4/upload', files=file_info, data=values)
+        self.client.post('/v1/file/upload', files=file_info, data=values)
+
+    def download(self):
         pass
 
 class PolicyService(HttpLocust):
